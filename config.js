ae1abff0bff21f1b64bfd1dfc9c23bf87490afac
spexy baba <baxsiyevmusviq357@gmail.com>
2024-02-17 01:08:46 +0400

    spexy baba

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    token: "", 
+    prefix: "!",
+  playing: 'discord.gg/FFGO#Community',
+    botdavet: "https://google.com/",
+    desteksunucusu: "https://google.com/",
+    website: "https://google.com/",
+    topgg: "https://google.com/",
+    politika: "https://google.com/",
+    sunucuid: "1172509191664050287",
+    destekçi: "1199359833271447633"
+    
+}
+
